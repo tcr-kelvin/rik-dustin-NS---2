e49b3bc86f4ddf9a56d7cb87b60394f63d013ee3
dgoldenlion@outlook.com <dgoldenlion@outlook.com>
2018-05-24 11:51:21 +0200

    header v.0.0.1

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -1,3 +1,18 @@
 <header>
-
+    <div class="logo-container">
+        <img src="https://photos-4.dropbox.com/t/2/AADZOXNtsZYIw8Gz63Uo6vm3CbdAGg8HlrVNpl0ZJl_OFA/12/158100610/png/32x32/1/_/1/2/logo.png/ELjj4ngYgKcHIAcoBw/WpSuSBJK4oKT__3KwOLwmvUimyo7FI6Mp84uC-IAOt4?preserve_transparency=1&size=1600x1200&size_mode=3" alt="TeleKing">
+    </div>
+    <nav>
+        <ul class="top-ul">
+            <li class="top-li"><a href="/">Home</a></li>
+            <li class="top-li"><a href="toestellen">Toestellen</a></li>
+            <li class="top-li"><a href="abonnementen">Abonnementen</a>
+                <ul class="in-ul">
+                    <li class="in-li"><a href="sim-only">Sim-Only</a></li>
+                    <li class="in-li"><a href="abonnement-toestel">Met toestel</a></li>
+                </ul>
+            </li>
+            <li class="top-li"><a href="support">Support & FAQ</a></li>
+        </ul>
+    </nav>
 </header>
