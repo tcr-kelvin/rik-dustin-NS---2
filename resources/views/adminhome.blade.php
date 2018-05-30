573161d21085df3cd1d482ac863ff1fc9f0ab3d0
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2018-05-30 14:58:18 +0200

    rik

--- resources/views/adminhome.blade.php
+++ resources/views/adminhome.blade.php
@@ -16,6 +16,38 @@
                         <div class="alert alert-success">
                             <p>You are logged in as admin</p>
                         </div>
+                            <table class="pull-left admin-table">
+                                <tbody>
+                                <tr>
+                                    <td class="bold">id</td>
+                                    <td class="bold">name</td>
+                                    <td class="bold">email</td>
+                                    <td class="bold">update</td>
+                                    <td class="bold">delete</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Dustin</td>
+                                    <td>info@duckerdie.com</td>
+                                    <td> <button class="table-button" type="button" name="button">update</button> </td>
+                                    <td> <button class="table-button" type="button" name="button">delete</button> </td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Rik</td>
+                                    <td>rik@gmail.com</td>
+                                    <td> <button class="table-button" type="button" name="button">update</button> </td>
+                                    <td> <button class="table-button" type="button" name="button">delete</button> </td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Annas</td>
+                                    <td>annas@hotmail.com</td>
+                                    <td> <button class="table-button" type="button" name="button">update</button> </td>
+                                    <td> <button class="table-button" type="button" name="button">delete</button> </td>
+                                </tr>
+                                </tbody>
+                            </table>
                     </div>
                 </div>
             </div>
--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -6,8 +6,42 @@
     @include ('layout.partials.header')
 
     <main>
-        <section>
-
+        <section class="section top-toestellen">
+            <div class="row">
+                <h2 class="section-title">Top Toestellen</h2>
+                <div class="column-4">
+                    <div class="item-box">
+                        <h4 class="item-title">Samsung Galaxy S9</h4>
+                        <img class="photo-box" src="photos/phone.png">
+                        <p class="item-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+                        <a class="item-button teleking-button" style="float:inherit;"href="content">Meer info</a>
+                    </div>
+                </div>
+                <div class="column-4">
+                    <div class="item-box">
+                        <h4 class="item-title">Apple iPhone X</h4>
+                        <img class="photo-box" src="photos/phone.png">
+                        <p class="item-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+                        <a class="item-button teleking-button" style="float:inherit;"href="content">Meer info</a>
+                    </div>
+                </div>
+                <div class="column-4">
+                    <div class="item-box">
+                        <h4 class="item-title">Samsung Galaxy S8</h4>
+                        <img class="photo-box" src="photos/phone.png">
+                        <p class="item-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+                        <a class="item-button teleking-button" style="float:inherit;"href="content">Meer info</a>
+                    </div>
+                </div>
+                <div class="column-4">
+                    <div class="item-box">
+                        <h4 class="item-title">OnePlus 6</h4>
+                        <img class="photo-box" src="photos/phone.png">
+                        <p class="item-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+                        <a class="item-button teleking-button" style="float:inherit;"href="content">Meer info</a>
+                    </div>
+                </div>
+            </div>
         </section>
     </main>
 
