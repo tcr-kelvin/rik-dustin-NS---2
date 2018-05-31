3fbafb691d3659cd1b7d520b4f1807272a23724c
ns <annas_amyaj@live.nl>
2018-05-31 15:21:01 +0200

    voor faker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Eloquent::ungard();
         // $this->call(UsersTableSeeder::class);
+        // $this->call(ProductTableSeeder);
     }
 }
