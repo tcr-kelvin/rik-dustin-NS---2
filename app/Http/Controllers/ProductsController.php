51f77be0d6141a354990c408b23e12d3fb90210b
dgoldenlion@outlook.com <dgoldenlion@outlook.com>
2018-05-31 09:46:58 +0200

    add crud code
    nog geen html

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductsStorePost;
+use App\Product;
+use Illuminate\Http\Request;
+
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('layout.', compact('products') );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductsStorePost $request)
+    {
+        //
+        $validated = $request->validated();
+
+        $product = new Product();
+        $product->name = $request['name'];
+        $product->description = $request['description'];
+        $product->imagepath = $request['imagepath'];
+        $product->price = $request['price'];
+        $product->save();
+
+        return redirect()->action('ProductsController@index')
+            ->with('status', 'Product toegevoegd');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     * products/6
+     */
+    public function show(Product $product)
+    {
+        //
+        return view('products.show', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+        return view('products.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductsStorePost $request, Product $product)
+    {
+        //
+        $validated = $request->validated();
+
+        //$product = new Product();
+        $product->name = $request['name'];
+        $product->description = $request['description'];
+        $product->imagepath = $request['imagepath'];
+        $product->price = $request['price'];
+        $product->save();
+
+        return redirect()->action('ProductsController@index')
+            ->with('status', 'Product gewijzigd');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+        $product->delete();
+        return redirect()->action('ProductsController@index')
+            ->with('status', 'Product verwijderd');
+    }
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -2622,6 +2622,7 @@ class ComposerStaticInit4ea96913e2cfaa0422429b137ad61075
         'PhpParser\\Parser\\Tokens' => __DIR__ . '/..' . '/nikic/php-parser/lib/PhpParser/Parser/Tokens.php',
         'PhpParser\\PrettyPrinterAbstract' => __DIR__ . '/..' . '/nikic/php-parser/lib/PhpParser/PrettyPrinterAbstract.php',
         'PhpParser\\PrettyPrinter\\Standard' => __DIR__ . '/..' . '/nikic/php-parser/lib/PhpParser/PrettyPrinter/Standard.php',
+        'ProductsTableSeeder' => __DIR__ . '/../..' . '/database/seeds/ProductsTableSeeder.php',
         'Prophecy\\Argument' => __DIR__ . '/..' . '/phpspec/prophecy/src/Prophecy/Argument.php',
         'Prophecy\\Argument\\ArgumentsWildcard' => __DIR__ . '/..' . '/phpspec/prophecy/src/Prophecy/Argument/ArgumentsWildcard.php',
         'Prophecy\\Argument\\Token\\AnyValueToken' => __DIR__ . '/..' . '/phpspec/prophecy/src/Prophecy/Argument/Token/AnyValueToken.php',
