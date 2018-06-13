515379ca57558a48932a8aed39d58bcc7662f5fd
dgoldenlion@outlook.com <dgoldenlion@outlook.com>
2018-06-13 15:41:51 +0200

    adminhome buttons

--- resources/views/adminhome.blade.php
+++ resources/views/adminhome.blade.php
@@ -22,41 +22,12 @@
         </div>
         <div class="row justify-content-center">
             <div class="column-m-10">
-
-                <table class="table table-bordered">
-                    <tbody>
-                    <tr>
-                        <td>id</td>
-                        <td>name</td>
-                        <td>description</td>
-                        <td>imagepath</td>
-                        <td>price</td>
-                        <td>CRUD</td>
-                    </tr>
-                    @foreach($products as $product)
-                        <tr>
-                            <td>{{$product->id}}</td>
-                            <td>{{$product->name}}</td>
-                            <td>{{substr($product->description,0,100).'...'}}</td>
-                            <td><img style="width:100px;"src=" {{$product->imagepath}}" alt="{{$product->name}}"></td>
-                            <td>{{$product->price}}</td>
-                            <td>
-                                <a href="{{route('product.show', $product->id)}}" class="btn btn-info btn-sm">
-                                    <i href="" class="fas fa-search"> </i>
-                                </a>
-                                <a href="{{route('product.edit', $product->id)}}" class="btn btn-primary btn-sm">
-                                    <i href="" class="fas fa-pencil-alt"> </i>
-                                </a>
-                                {!! Form::open(['method' => 'DELETE','route' => ['product.destroy', $product->id],'style'=>'display:inline']) !!}
-                                <button type="submit" style="display: inline;" class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></button>
-                                {!! Form::close() !!}
-                            </td>
-                        </tr>
-                    @endforeach
-
-                    </tbody>
-
-                </table>
+                <form action="home/product">
+                    <input class="btn btn-success" type="submit" value="Producten" />
+                </form>
+                <form action="home/user">
+                    <input class="btn btn-success"  type="submit" value="Gebruikers" />
+                </form>
             </div>
         </div>
 
