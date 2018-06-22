c069f0827e84dca518d67d4a3396f4fc4d72694f
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2018-06-22 13:19:09 +0200

    Revert "Revert "Merge branch 'development' of https://github.com/tcr-kelvin/rik-dustin-NS---2 into development""
    This reverts commit a9d99d35334ef09faf0fc2bf7ac0d0ad671e4f28.

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -6,21 +6,24 @@
     @include ('layout.partials.header')
 
     <main>
+
         <section class="section top-toestellen">
-            <div class="row">
-                <h2 class="section-title">Top Toestellen</h2>
+            <h2 class="section-title">Top Toestellen</h2>
+            <div class="row-box">
                 @foreach($products->take(4) as $product)
-                <div class="column-4">
                     <div class="item-box">
                         <h4 class="item-title">{{$product->name}}</h4>
                         <img class="photo-box" src="{{$product->imagepath}}">
                         <p class="item-info">{{substr($product->description,0,100).'...'}}</p>
-                        <a class="item-button teleking-button" style="float:inherit;"href="content">Meer info</a>
+                        <a class="item-button teleking-button" style="float:inherit;"href="product/{{$product->id}}"><strong>€{{$product->price}} </strong>- Meer info</a>
                     </div>
-                </div>
                 @endforeach
             </div>
         </section>
+        <div class="paralaximg">
+            <h1>TeleKing</h1>
+            <span>Telefoons en Abonnementen</span>
+        </div>
     </main>
 
     {{--footer--}}
