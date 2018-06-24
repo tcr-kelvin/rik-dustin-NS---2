15905a04e29d81979d7704d9f299234791d7bcab
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2018-06-24 17:32:33 +0200

    v2

--- resources/views/simonly.blade.php
+++ resources/views/simonly.blade.php
@@ -47,7 +47,7 @@
                         </th>
                     </tr>
                     <?php $no = 1; ?>
-                    @foreach($users as $key => $value)
+                    @foreach($simonlys as $key => $value)
                         <tr>
                             <td>{{$value->id}}</td>
                             <td>{{ $value->name }}</td>
@@ -71,30 +71,4 @@
             </div>
         </div>
     </div>
-    <script>
-        $(document).ready(function(){
 
-            $('#country_name').keyup(function(){
-                var query = $(this).val();
-                if(query != '')
-                {
-                    var _token = $('input[name="_token"]').val();
-                    $.ajax({
-                        url:"{{ route('user.fetch') }}",
-                        method:"POST",
-                        data:{query:query, _token:_token},
-                        success:function(data){
-                            $('#countryList').fadeIn();
-                            $('#countryList').html(data);
-                        }
-                    });
-                }
-            });
-
-            $(document).on('click', 'li', function(){
-                $('#country_name').val($(this).text());
-                $('#countryList').fadeOut();
-            });
-
-        });
-    </script>
