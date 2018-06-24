018ff17fd4aaf33ed458eb8a7d65497bb3eecf6f
Dustin <dgoldenlion@outlook.com>
2018-06-25 00:11:39 +0200

    rem "autorise"

--- app/Http/Requests/ProductsStorePost.php
+++ app/Http/Requests/ProductsStorePost.php
@@ -13,9 +13,9 @@ class ProductsStorePost extends FormRequest
      */
     public function authorize()
     {
-        if (auth::user()) {
+//        if (auth::user()) {
             return true;
-        }
+//        }
 
     }
 
