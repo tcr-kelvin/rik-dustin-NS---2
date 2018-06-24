efbb36691c7d7dd8b829956b7ba61e363011dd3f
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2018-06-24 16:18:07 +0200

    delete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,50 +18,6 @@
                     </div>
                 </div>
             </div>
-            <div class="row justify-content-center">
-                <div class="row">
-                    <div class="col-md-6 col-md-offset-3">
-                        {{ Form::model($user,['route'=>['user.update',$user->id],'method'=>'PATCH']) }}
-                        @include('grud.form_master')
-                        {{ Form::close() }}
-                    </div>
-                    <div class="col-sm-2">
-                        {!! form::label('name','name') !!}
-                    </div>
-                    <div class="col-sm-10">
-                        <div class="form-group {{ $errors->has('name') ? 'has-error' : "" }}">
-                            {{ form::text('name',NULL, ['class'=>'form-control', 'id'=>'name', 'placeholder'=>'name']) }}
-                            {{ $errors->first('name', '<p class="help-block">:message</p>') }}
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-sm-2">
-                        {!! form::label('email','email') !!}
-                    </div>
-                    <div class="col-sm-10">
-                        <div class="form-group {{ $errors->has('email') ? 'has-error' : "" }}">
-                            {{ form::text('email',NULL, ['class'=>'form-control', 'id'=>'email', 'placeholder'=>'email']) }}
-                            {{ $errors->first('email', '<p class="help-block">:message</p>') }}
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-2">
-                        {!! form::label('password','password') !!}
-                    </div>
-                    <div class="col-sm-10">
-                        <div class="form-group {{ $errors->has('password') ? 'has-error' : "" }}">
-                            {{ form::text('password',NULL, ['class'=>'form-control', 'id'=>'password', 'placeholder'=>'password']) }}
-                            {{ $errors->first('password', '<p class="help-block">:message</p>') }}
-                        </div>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    {{ form::button(isset($model)? 'Update' : 'save' , ['class'=>'btn btn-success', 'type'=>'submit']) }}
-                </div>
             </div>
         </div>
     </div>
