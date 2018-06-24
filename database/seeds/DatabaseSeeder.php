727a2ca1d9959fc07cc06d6338b8fe0cbf098653
Dustin <dgoldenlion@outlook.com>
2018-06-24 16:36:15 +0200

    revert kut

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,11 +13,16 @@ class DatabaseSeeder extends Seeder
     {
         //Eloquent::ungard();
 <<<<<<< HEAD
+<<<<<<< HEAD
 //        $this->call(UsersTableSeeder::class);
 //        $this->call(ProductsTableSeeder::class);
 =======
         //$this->call(UsersTableSeeder::class);
         //$this->call(ProductsTableSeeder::class);
 >>>>>>> parent of 1de30b0... jwf
+=======
+        $this->call(UsersTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
+>>>>>>> parent of b04b758... kut
     }
 }
