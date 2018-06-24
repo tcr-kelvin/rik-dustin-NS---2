e43700d2a4f1c41324c884ed1c8d478a1050d677
Dustin <dgoldenlion@outlook.com>
2018-06-25 00:04:27 +0200

    jef

--- database/seeds/QuestionsTableSeeder.php
+++ database/seeds/QuestionsTableSeeder.php
@@ -11,6 +11,9 @@ class QuestionsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('questions')->insert([
+            'title' => str_random(10),
+            'description' => str_random(200),
+        ]);
     }
 }
