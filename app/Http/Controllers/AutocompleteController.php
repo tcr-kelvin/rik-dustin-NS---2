00bd265eef690f36aefe5d8a96c7daeac121e090
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2018-06-26 12:15:44 +0200

    fix live search

--- app/Http/Controllers/AutocompleteController.php
+++ app/Http/Controllers/AutocompleteController.php
@@ -42,9 +42,7 @@ class AutocompleteController extends Controller
             <td><a href="#">'.$row->password.'</a></td>
             <td><a href="#">'.$row->admin.'</a></td>
             <td>
-                                <a href="'{{route('user.show', $value->id)}}'" class="btn btn-info btn-sm">
-                                    <i href="" class="fas fa-search"> </i>
-                                </a>
+                               
            
                                
                              
